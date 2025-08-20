ebca4912881dca80fbf77b744bee116f28492b93
TonyCross23 <tonychamp40@gmail.com>
2025-08-20 20:07:17 +0630

    docker

--- dist/api/auth/utils/appConst.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.INTERFACE_TYPE = void 0;
-exports.INTERFACE_TYPE = {
-    PrismaService: Symbol.for("PrismaService"),
-    AuthRepository: Symbol.for("AuthRepository"),
-    AuthController: Symbol.for("AuthController"),
-    AuthInteractor: Symbol.for("AuthInteractor")
-};
--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import morgan from "morgan";
 import log from "./logger/log";
 import router from "./router";
 import { errorHandler } from "./middlewares/appError";
+import bodyParser from 'body-parser';
 
 const PORT = process.env.PORT || 6000;
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(morgan("dev", {
   stream: {
