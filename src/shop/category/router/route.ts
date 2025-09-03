423b1558362896fe146a525ecdd961b67d800fc9
webgipsy <babybird1801@gmail.com>
2025-09-03 11:59:11 +0630

    adding auth in customer & category

--- src/shop/category/router/route.ts
+++ src/shop/category/router/route.ts
@@ -3,13 +3,14 @@ import { Router } from "express";
 import { CategoryController } from "../controller/CategoryController";
 import { INTERFACE_TYPE } from "../utils";
 import container from "../config/container";
+import { authMiddleware } from "../../../middlewares/authMiddleware";
 
 
 const categoryRoute = Router()
 
 const controller = container.get<CategoryController>(INTERFACE_TYPE.CategoryController)
 
-categoryRoute.post('/create', controller.createCategory)
+categoryRoute.post('/create', authMiddleware, controller.createCategory)
 categoryRoute.get('/', controller.getAllCategory)
 
 export default categoryRoute
